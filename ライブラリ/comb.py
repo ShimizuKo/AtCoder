f2173b3cf8b19e043ebe3c877b1fba0ad4449932
ShimizuKo <thebeatles0502@gmail.com>
2020-01-13 00:48:54 +0900

    created comb in ライブラリ

---
+++ ライブラリ/comb.py
@@ -0,0 +1,17 @@
+N = 100
+MOD = 10 ** 9 + 7
+#変にエラー吐かれないようにするための変数
+
+kai1 = [1, 1]
+kai2 = [1, 1]
+inverse = [0, 1]
+for i in range(2, N + 1):
+  kai1.append((kai1[-1] * i) % MOD)
+  inverse.append((-inverse[MOD % i] * (MOD // i)) % MOD)
+  kai2.append((kai2[-1] * inverse[-1]) % MOD)
+
+def comb(n, r):
+  if r < 0 or r > n:
+    return 0
+  r = min(r, n - r)
+  return (kai1[n] * kai2[r] * kai2[n - r]) % MOD
